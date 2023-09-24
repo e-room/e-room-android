1d8312252b13e96aabd8b7b7c6a8711f0b4dabb9
DongChyeon <kimdonghyun90766@gmail.com>
2023-09-24 20:22:27 +0900

    📝 Feat : GPS 기능 요청 시 위치 권한 요청 팝업창 뜨게 함

--- app/src/main/java/com/dongchyeon/e_room/MainActivity.kt
+++ app/src/main/java/com/dongchyeon/e_room/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.dongchyeon.e_room
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.webkit.ConsoleMessage
 import android.webkit.GeolocationPermissions
 import android.webkit.WebChromeClient
 import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.dongchyeon.e_room.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var webView: WebView
     private lateinit var onBackPressedCallback: OnBackPressedCallback
+
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +33,7 @@ class MainActivity : AppCompatActivity() {
         webView = binding.webview
 
         with(webView) {
-            settings.javaScriptEnabled = true
-            settings.setGeolocationEnabled(true)
+            //loadUrl("https://m.map.naver.com/")
             loadUrl("https://www.e-room.app/")
 
             webViewClient = object : WebViewClient() {
@@ -45,8 +52,19 @@ class MainActivity : AppCompatActivity() {
                 ) {
                     super.onGeolocationPermissionsShowPrompt(origin, callback)
                     callback?.invoke(origin, true, false)
+                    requestLocationPermission()
+                }
+
+                override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+                    consoleMessage?.apply {
+                        Log.d("e-room", "${message()} -- From line ${lineNumber()} of ${sourceId()}")
+                    }
+                    return true
                 }
             }
+
+            settings.javaScriptEnabled = true
+            settings.setGeolocationEnabled(true)
         }
 
         onBackPressedCallback = object : OnBackPressedCallback(true) {
@@ -59,5 +77,30 @@ class MainActivity : AppCompatActivity() {
             }
         }
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+    }
+
+    private fun isLocationPermissionGranted(): Boolean {
+        val coarseLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+        val fineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        return coarseLocationPermission == PackageManager.PERMISSION_GRANTED && fineLocationPermission == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestLocationPermission() {
+        val permissions = arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+        ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (!isLocationPermissionGranted()) {
+                Toast.makeText(
+                    applicationContext,
+                    "위치 권한을 허용해야 현재 위치를 알 수 있습니다.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
